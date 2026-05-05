7141adff520b54d3cb1fa2650154d47253ae41f0
duudafs <dudascomparin@gmail.com>
2026-05-05 14:58:31 -0300

    treinando para pegar api

--- actions/actions.py
+++ actions/actions.py
@@ -7,10 +7,34 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from datetime import datetime, timedelta
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+import requests
+
+class ActionCotacaoDolar(Action):
+
+    def name(self):
+        return "action_cotacao_dolar"
+
+    def run(self, dispatcher, tracker, domain):
+        ontem = (datetime.now() - timedelta(days=1)).strftime("%m-%d-%Y")
+
+        url = f"https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='{ontem}'&$format=json"
+
+        response = requests.get(url)
+        data = response.json()
+
+        if "value" in data and len(data["value"]) > 0:
+            cotacao = data["value"][0]["cotacaoVenda"]
+            dispatcher.utter_message(text=f"dólar: R$ {cotacao:.2f}")
+        else:
+            dispatcher.utter_message(text="não consegui pegar a cotação")
+
+        return []
 #
 #
 # class ActionHelloWorld(Action):
